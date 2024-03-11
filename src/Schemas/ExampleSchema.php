952010935252c71e96e652ef240d5eb30d4e62d5
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-03-11 19:30:47 +0100

    ExampleSchema: Add other datatypes
    This is meant to make testing easier. The fields do not make any
    sense, but it is also an example schema, so it does not matter
    that much.

--- src/Schemas/ExampleSchema.php
+++ src/Schemas/ExampleSchema.php
@@ -8,4 +8,16 @@ class ExampleSchema implements JsonSchema {
 	public const CCExampleBackgroundColor = [
 		self::TYPE => self::TYPE_STRING
 	];
+
+	public const CCExampleBool = [
+		self::TYPE => self::TYPE_BOOLEAN,
+	];
+
+	public const CCExampleString = [
+		self::TYPE => self::TYPE_STRING,
+	];
+
+	public const CCExampleNumber = [
+		self::TYPE => self::TYPE_NUMBER,
+	];
 }
