8169d3d25a499f9f085e177ad3207b44aede5ea8
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-04-25 10:42:04 +0200

    ExampleSchema: Add default values
    Bug: T363426

--- src/Schemas/ExampleSchema.php
+++ src/Schemas/ExampleSchema.php
@@ -6,18 +6,22 @@ use MediaWiki\Extension\CommunityConfiguration\Schema\JsonSchema;
 
 class ExampleSchema extends JsonSchema {
 	public const CCExampleBackgroundColor = [
-		self::TYPE => self::TYPE_STRING
+		self::TYPE => self::TYPE_STRING,
+		self::DEFAULT => 'red',
 	];
 
 	public const CCExampleBool = [
 		self::TYPE => self::TYPE_BOOLEAN,
+		self::DEFAULT => false,
 	];
 
 	public const CCExampleString = [
 		self::TYPE => self::TYPE_STRING,
+		self::DEFAULT => '',
 	];
 
 	public const CCExampleNumber = [
 		self::TYPE => self::TYPE_NUMBER,
+		self::DEFAULT => 0,
 	];
 }
